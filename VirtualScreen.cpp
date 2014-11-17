a375c6514af2250558be95f2fdae8853887ebdcf
Aymeric GERLIER <aymeric.gerlier@sightcall.com>
2014-11-17 17:57:40 +0100

    virtualscreen skeleton

--- VirtualScreen.cpp
+++ VirtualScreen.cpp
@@ -9,11 +9,81 @@
 
 
 
-/*
-void VirtualScreen::init(const byte pin, const prog_char* label)
+
+VirtualScreen::VirtualScreen()
 {
-  this->_id.pin = pin;
-  this->_label  = label;
-  pinMode(this->getPin(), OUTPUT);
+  $this->_tab  = 0;
+  $this->_itr = NULL;
 }
-*/
+
+
+void VirtualScreen::previousTab()
+{
+  if (idx == 0) {
+    VirtualScreen::_tab = ARRAYLEN(Core::groups);
+  }
+  VirtualScreen::_tab--;
+}
+
+
+void VirtualScreen::nextTab()
+{
+  VirtualScreen::_tab++;
+  if (idx == ARRAYLEN(Core::groups)) {
+    VirtualScreen::_tab = 0;
+  }
+}
+
+
+void VirtualScreen::previousMessage()
+{
+    Warehouse_FOREACHPP(Pinout, Core::groups[this->_tab]->pinouts, element)
+      Core::statusLineToBuffer(element);
+      if (this->_itr == element->next) {
+        return this->_itr == element;
+      }
+    Warehouse_ENDFOREACHPP
+}
+
+
+void VirtualScreen::nextMessage()
+{
+  if (NULL != this->_node->next) {
+    this->_itr = this->_itr->next;
+  } else {
+    this->_itr = Core::groups[this->_tab]->pinouts->begin();
+  }
+}
+
+
+void VirtualScreen::nextTab()
+{
+  VirtualScreen::_tab++;
+  if (idx == ARRAYLEN(Core::groups)) {
+    VirtualScreen::_tab = 0;
+  }
+}
+
+const prog_char* VirtualScreen::getTitle()
+{
+  switch(Core::groups[this->_tab]->type) {
+    
+  }
+  return PSTR("TODO");
+}
+
+Pinout* VirtualScreen::getPinout()
+{
+  return $this->_itr->item;
+}
+
+const uint8_t VirtualScreen::getPageNumber()
+{
+  return 0;
+}
+
+
+const uint8_t VirtualScreen::getLastPageNumber()
+{
+  return Core::groups[this->_tab]->pinouts->size();
+}
--- VirtualScreen.h
+++ VirtualScreen.h
@@ -5,29 +5,63 @@
 
 #include <Arduino.h>
 #include <avr/pgmspace.h>
+#include "macros.h"
 #include "Core.h"
 
-/*
+
+#define SMASK_TAB(bookmark)   bookmark >> 6
+#define SMASK_PAGE(bookmark)  bookmark & B111111
+#define SMASK_TAB_INC         B1000000
+#define SMASK_PAGE_INC        B1
+
+
 class VirtualScreen {
 
   public:
-  static void previousTab();
-  __attribute__((always_inline)) inline static void nextTab();
-  static void previousPage();
-  __attribute__((always_inline)) inline static void nextPage();
+  VirtualScreen();
 
-  static void getTitle();
-  __attribute__((always_inline)) inline static void getPinout() {};
+  void previousTab();
+  void nextTab();
+  void previousPage();
+  void nextPage();
 
-  static const uint8_t getPage();
-  static const uint8_t getLastPage();
+  const prog_char* getTitle();
+  Pinout* getPinout() {};
+
+  __attribute__((always_inline)) inline const uint8_t getTabNumber();
+  __attribute__((always_inline)) inline const uint8_t getLastTabNumber();
+
+  const uint8_t getPageNumber();
+  const uint8_t getLastPageNumber();
 
   protected:
-  static uint8_t _tab;
-  static Pinout* _pinout;
+  uint8_t _bookmark;
+  uint8_t _tab;
+  Warehouse<T*>::iterator _itr;
 
 };
-*/
+
+
+
+
+/***********************************************************
+ *                         INLINE                          *
+ **********************************************************/
+
+
+__attribute__((always_inline)) inline
+const uint8_t VirtualScreen::getTabNumber()
+{
+  return 1 + this->_tab;
+}
+
+
+__attribute__((always_inline)) inline
+const uint8_t VirtualScreen::getLastTabNumber()
+{
+  return ARRAYLEN(Core::groups);
+}
+
 
 
 #endif
